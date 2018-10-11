dba8c372f9e7d49499801f903ba777e12abad2df
David Joel McGarvey <davidjmcgarvey@gmail.com>
2018-10-11 15:55:26 -0700

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -13,17 +13,17 @@ import random
 
 
 def start_game():
-    random_number = random.randint(1, 10)
+    random_number = int(random.randint(1, 10))
     print("Hello traveler! Welcome to the game of guesses!")
     player_name = input("What is your name? ")
     wanna_play = input("Hi, {}, would you like to play the guessing game? ".format(player_name))
     if wanna_play.lower() == "yes":
         guess = input("Pick a number between 1 and 10 ")
-        if int(guess) == int(random_number):
+        if int(guess) == random_number:
             print("Nice! You got it!")
-        elif int(guess) > int(random_number):
+        elif int(guess) > random_number:
             print("It's higher")
-        elif int(guess) < int(random_number):
+        elif int(guess) < random_number:
             print("It's lower")
     else:
         print("That's cool, have a good one!")
