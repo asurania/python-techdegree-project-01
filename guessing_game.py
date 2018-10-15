4b5472fd29978d41ea1be3eff7efdee9e83c5592
David Joel McGarvey <davidjmcgarvey@gmail.com>
2018-10-15 13:14:31 -0700

    Added Exceeds requirments for TD

--- guessing_game.py
+++ guessing_game.py
@@ -16,22 +16,29 @@ def start_game():
     attempts = 1
     print("Hello traveler! Welcome to the game of guesses!")
     player_name = input("What is your name? ")
-    wanna_play = input("Hi, {}, would you like to play the guessing game? ".format(player_name))
+    wanna_play = input("Hi, {}, would you like to play the guessing game? (Enter Yes/No) ".format(player_name))
     while wanna_play.lower() == "yes":
         try:
             guess = input("Pick a number between 1 and 10 ")
+            if int(guess) < 1 or int(guess) > 10:
+                raise ValueError("Please guess a number within the given range")
             if int(guess) == random_number:
                 print("Nice! You got it!")
                 print("It took you {} attempts".format(attempts))
-                break
+                play_again = input("Would you like to play again? (Enter Yes/No) ")
+                attempts = 1
+                random_number = int(random.randint(1, 10))
+                if play_again.lower() == "no":
+                    break
             elif int(guess) > random_number:
                 print("It's lower")
                 attempts += 1
             elif int(guess) < random_number:
                 print("It's higher")
                 attempts += 1
-        except ValueError:
+        except ValueError as err:
             print("Oh no!, that is not a valid value. Try again...")
+            print("({})".format(err))
     else:
         print("That's cool, have a good one!")
 
