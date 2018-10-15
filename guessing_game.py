fe755976ad2ef03de2f7163b0f57979ba2d15b02
David Joel McGarvey <davidjmcgarvey@gmail.com>
2018-10-15 13:07:28 -0700

    update workspace

--- guessing_game.py
+++ guessing_game.py
@@ -3,14 +3,13 @@ Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
 
-For this first project we will be using Workspaces. 
+For this first project we will be using Workspaces.
 
 NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
 
 """
 
 import random
-
 
 def start_game():
     random_number = int(random.randint(1, 10))
@@ -19,25 +18,26 @@ def start_game():
     player_name = input("What is your name? ")
     wanna_play = input("Hi, {}, would you like to play the guessing game? ".format(player_name))
     while wanna_play.lower() == "yes":
-        guess = input("Pick a number between 1 and 10 ")
-        if int(guess) == random_number:
-            attempts += 1
-            print("Nice! You got it!")
-            print("It took you {} attempts".format(attempts))
-            break
-        elif int(guess) > random_number:
-            print("It's lower")
-            attempts += 1
-        elif int(guess) < random_number:
-            print("It's higher")
-            attempts += 1
+        try:
+            guess = input("Pick a number between 1 and 10 ")
+            if int(guess) == random_number:
+                print("Nice! You got it!")
+                print("It took you {} attempts".format(attempts))
+                break
+            elif int(guess) > random_number:
+                print("It's lower")
+                attempts += 1
+            elif int(guess) < random_number:
+                print("It's higher")
+                attempts += 1
+        except ValueError:
+            print("Oh no!, that is not a valid value. Try again...")
     else:
         print("That's cool, have a good one!")
 
 
-    """
-    Psuedo-code Hints
-    
+    """Psuedo-code Hints
+
     When the program starts, we want to:
     ------------------------------------
     1. Display an intro/welcome message to the player.
@@ -45,14 +45,15 @@ def start_game():
     3. Continuously prompt the player for a guess.
       a. If the guess greater than the solution, display to the player "It's lower".
       b. If the guess is less than the solution, display to the player "It's higher".
-    
+
     4. Once the guess is correct, stop looping, inform the user they "Got it"
          and show how many attempts it took them to get the correct number.
     5. Let the player know the game is ending, or something that indicates the game is over.
-    
+
     ( You can add more features/enhancements if you'd like to. )
     """
     # write your code inside this function.
+
 
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
