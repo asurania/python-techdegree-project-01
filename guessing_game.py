3521947d5f2e7f8045f7bdb5ee5f90d7efe6aaa5
David Joel McGarvey <davidjmcgarvey@gmail.com>
2018-10-15 13:34:54 -0700

    Added show_score function

--- guessing_game.py
+++ guessing_game.py
@@ -2,11 +2,6 @@
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
 --------------------------------
-
-For this first project we will be using Workspaces.
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
 
 import random
@@ -17,6 +12,9 @@ def start_game():
     print("Hello traveler! Welcome to the game of guesses!")
     player_name = input("What is your name? ")
     wanna_play = input("Hi, {}, would you like to play the guessing game? (Enter Yes/No) ".format(player_name))
+    def show_score():
+        print("The current high score is {} attempt(s)".format(attempts))
+    show_score()
     while wanna_play.lower() == "yes":
         try:
             guess = input("Pick a number between 1 and 10 ")
@@ -26,6 +24,7 @@ def start_game():
                 print("Nice! You got it!")
                 print("It took you {} attempts".format(attempts))
                 play_again = input("Would you like to play again? (Enter Yes/No) ")
+                show_score()
                 attempts = 1
                 random_number = int(random.randint(1, 10))
                 if play_again.lower() == "no":
@@ -42,26 +41,5 @@ def start_game():
     else:
         print("That's cool, have a good one!")
 
-
-    """Psuedo-code Hints
-
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
-
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
     start_game()
